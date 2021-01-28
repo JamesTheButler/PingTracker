43c72fe5d5c75a26b915c79f4be8958dfb6587cb
Tom <tom.ille@gmx.de>
2021-01-28 14:17:44 +0100

    dynamic horizontal and vertical lines, based on graph sizes; colors are in dict; ping status in enum

--- pingtracker.py
+++ pingtracker.py
@@ -1,17 +1,43 @@
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FormatStrFormatter as strFormatter
 import numpy as np
-from numpy import random
 from collections import deque
 from tcp_latency import measure_latency
-import threading
-  
+import signal
+from enum import Enum
+
+class PingStatus(Enum):
+    NONE = 0
+    GOOD = 1
+    OK = 2
+    BAD = 3
+
 maxGoodPing = 50
 minBadPing = 100
-entryCount = 50
+entryCount = 1200
 pingInterval = 1
 timeout = 500
 host = "8.8.8.8"
+vertLineInterval = 200
+horiLineInterval = (int) (timeout / 5)
+
+fullColor = {
+    PingStatus.NONE : '#000000',
+    PingStatus.GOOD : '#24a60f',
+    PingStatus.OK : '#c7db14',
+    PingStatus.BAD : '#db1e14',
+}
+lightColor = {
+    PingStatus.NONE : '#3d3d3d',
+    PingStatus.GOOD : '#7fc973',
+    PingStatus.OK : '#f0eaaf',
+    PingStatus.BAD : '#d67f7a',
+}
+
+yMin = 0 - (int)(timeout/20)
+yMax = timeout + (int)(timeout/20)
+xMin = entryCount
+xMax = 0 - (int)(entryCount/50)
 
 def GetPing():
     ping = measure_latency(host = host, runs=1, timeout = timeout/1000)[0]
@@ -20,70 +46,81 @@ def GetPing():
     else:
         return round(ping)
 
-def GetPlotColor(ping):
-    if ping>maxGoodPing:
-        if ping>minBadPing:
-            return 'r'
+def GetPingStatus(ping):
+    if ping > maxGoodPing:
+        if ping > minBadPing:
+            return PingStatus.BAD
         else:
-            return 'y'
+            return PingStatus.OK
     else: 
-        return 'g'
+        return PingStatus.GOOD
+
+def AddFillTrace(ping):
+    pass
 
 def Main():
-    timeScale = range(0, entryCount)
-    pingEntries = deque(np.full(entryCount, 0))
-
+    # remove matplotlib toolbar
     plt.rcParams['toolbar'] = 'None' 
     
+    # set window title
     fig = plt.gcf()
     fig.canvas.set_window_title('Pinging ' + host)
 
     # plot limits
-    plt.ylim(0, timeout+10)
-    plt.xlim(50, 0-1)
-    # vertical lines
-    plt.axvline(x=0, color= '#808080', linewidth = 0.5)
-    plt.axvline(x=10, color= '#808080', linewidth = 0.5)
-    plt.axvline(x=20, color= '#808080', linewidth = 0.5)
-    plt.axvline(x=30, color= '#808080', linewidth = 0.5)
-    plt.axvline(x=40, color= '#808080', linewidth = 0.5)
-    # horizontal lines
-    plt.axhline(y=100, color= '#808080', linewidth = 0.5)
-    plt.axhline(y=200, color= '#808080', linewidth = 0.5)
-    plt.axhline(y=300, color= '#808080', linewidth = 0.5)
-    plt.axhline(y=400, color= '#808080', linewidth = 0.5)
-    # threshold lines
-    #plt.axhline(y=maxGoodPing, color= '#bfc257', linewidth = 1)
-    #plt.axhline(y=minBadPing, color= '#e37f7f', linewidth = 1)
+    plt.ylim(yMin, yMax)
+    plt.xlim(xMin, xMax)
 
+    # vertical and horizontal lines
+    for i in range(0, yMax, horiLineInterval):
+        plt.axhline(y=i, color= '#808080', linewidth = 0.5)
+    for i in range(0, xMin, vertLineInterval):
+        plt.axvline(x=i, color= '#808080', linewidth = 0.5)
+    
+    # add seconds/milliseconds to axes
     plt.gca().xaxis.set_major_formatter(strFormatter('%d s'))
     plt.gca().yaxis.set_major_formatter(strFormatter('%d ms'))
 
-
+    #plt.show()
+    interrupted = False
+    #def signal_handler(signal, frame):
+    #    global interrupted
+    #    print("exiting")
+    #    interrupted = True
+#
+    #signal.signal(signal.SIGINT, signal_handler)
+    
+    timeScale = range(0, entryCount)
+    pingEntries = deque(np.full(entryCount, 0))
     while True:
         currentPing = GetPing()
 
         pingEntries.rotate(1)
         pingEntries[0] = currentPing
-        plotColor = GetPlotColor(currentPing)
+        pingStatus = GetPingStatus(currentPing)
 
-        pingText = plt.text(5, 480, str(currentPing) + ' ms', color = plotColor, fontweight='bold')
-        line, = plt.plot(timeScale, pingEntries, color = plotColor, linewidth = 3) 
-        point = plt.scatter(timeScale[0], pingEntries[0], color = plotColor)
+        pingText = plt.text(5, timeout-20, str(currentPing) + ' ms', color = fullColor[pingStatus], fontweight='bold')
+        line, = plt.plot(timeScale, pingEntries, color = fullColor[pingStatus], linewidth = 3) 
+        liveFill = plt.fill_between(timeScale, 0, pingEntries, color = lightColor[pingStatus])
+        point = plt.scatter(timeScale[0], pingEntries[0], color = fullColor[pingStatus])
+
         plt.pause(pingInterval - (currentPing/1000))
+
         line.remove()
         del line
         point.remove()
         del point
         pingText.remove()
         del pingText
+        liveFill.remove()
+        del liveFill
+
+        if interrupted:
+            print("Exit.")
+            break
+    
+    plt.close()
+
+
 
 ##############################
-
-#b = threading.Thread(name='background', target=Main)
-#f = threading.Thread(name='foreground', target=foreground)
-#
-#b.start()
-#f.start()
-#
 Main()
